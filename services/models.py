b7cf20b552d29a3de957da614d0304a15cf98e21
thebards <swordbookstory@gmail.com>
2023-08-28 14:31:44 +0000

    update

--- services/models.py
+++ services/models.py
@@ -11,7 +11,7 @@ class Models:
 	@classmethod
 	def load(cls):
 		model_path = os.environ.get("EMBEDDING_MODEL_PATH")
+		# load model
+		logger.info(f"Load embedding model from {model_path} {os.listdir(model_path)}")
 		if model_path:
-			# load model
-			logger.info(f"Load embedding model from {model_path}")
 			cls.embedder = SentenceTransformer(model_path)
